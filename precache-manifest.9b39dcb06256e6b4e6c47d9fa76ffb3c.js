fb7bf546e5c8f7279a66bec3cc618f5e3a19beaf
david-mateogit <davidmateo@outlook.com>
2019-10-03 10:22:43 -0400

    Updates

---
+++ precache-manifest.9b39dcb06256e6b4e6c47d9fa76ffb3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f5f0a37edcca52da9b97a257eeb51a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a747081a1c7f0d8c0f15",
+    "url": "/monsters-rolodex/static/css/main.006e8493.chunk.css"
+  },
+  {
+    "revision": "d2282100f3ee5fe83ba8",
+    "url": "/monsters-rolodex/static/js/2.a3e63e5f.chunk.js"
+  },
+  {
+    "revision": "a747081a1c7f0d8c0f15",
+    "url": "/monsters-rolodex/static/js/main.4a5566ad.chunk.js"
+  },
+  {
+    "revision": "3e934bba1715730f25fd",
+    "url": "/monsters-rolodex/static/js/runtime-main.f25096c2.js"
+  }
+]);
